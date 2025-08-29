d57c93e697b9f8e0dc7d52ede087a6eedac127de
js <jrsweb@proton.me>
2025-08-29 08:32:36 +0200

    Sprog i admin rettet

--- assets/apps.py
+++ assets/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class AssetsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'assets'
+    verbose_name = "Aktiver"  # Dette navn vises i admin-siden
--- vprepair/settings.py
+++ vprepair/settings.py
@@ -14,7 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'assets',
+    'assets.apps.AssetsConfig',
 ]
 
 MIDDLEWARE = [
